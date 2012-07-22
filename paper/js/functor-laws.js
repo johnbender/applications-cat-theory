d887c8f95f5be012c650e189877b945fef35879c
John Bender <john.m.bender@gmail.com>
2012-07-22 13:53:46 -0700

    small bits of formatting more TODO's

--- paper/js/functor-laws.js
+++ paper/js/functor-laws.js
@@ -2,5 +2,5 @@
 @\label{lst-line:functor-law-id}@jQuery(id(elem)) == jQuery(elem).id();
 
 // preservation of composition
-@\label{lst-line:functor-law-cmps}@functor(compose(f, g))
-  == jQuery.compose(functor(f), functor(g));
+@\label{lst-line:functor-law-cmps}@functor(cmps(f, g))
+  == jQuery.cmps(functor(f), functor(g));
