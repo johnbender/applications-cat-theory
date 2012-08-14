64052cec67e3f7c84cacca9d0c0e163681c66423
John Bender <john.m.bender@gmail.com>
2012-08-13 23:47:11 -0700

    use paper.pdf in gh-pages

--- bin/gh-pages.sh
+++ bin/gh-pages.sh
@@ -1,9 +1,11 @@
 set -e
 
 cp -R poster /tmp/
+cp paper/paper.pdf /tmp/paper.pdf
 git checkout gh-pages
 cp -Rf /tmp/poster/* .
 mv poster.html index.html
+cp /tmp/paper.pdf paper-`date +%Y-%m`.pdf
 git add .
 git commit -m "update gh-pages"
 git checkout master
