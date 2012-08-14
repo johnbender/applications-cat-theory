c9c332d9a07f3526e74bfc7f26b39712554b5ac6
John Bender <john.m.bender@gmail.com>
2012-08-13 23:52:03 -0700

    simplify gh-pages

--- bin/gh-pages.sh
+++ bin/gh-pages.sh
@@ -1,11 +1,10 @@
 set -e
 
 cp -R poster /tmp/
-cp paper/paper.pdf /tmp/paper.pdf
 git checkout gh-pages
 cp -Rf /tmp/poster/* .
 mv poster.html index.html
-cp /tmp/paper.pdf paper-`date +%Y-%m`.pdf
+cp paper/paper.pdf paper-`date +%Y-%m`.pdf
 git add .
 git commit -m "update gh-pages"
 git checkout master
