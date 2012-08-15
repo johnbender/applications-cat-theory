f5e2ecbf263484b9ac782c96f275a0ff7d7418ff
John Bender <john.m.bender@gmail.com>
2012-08-15 00:13:44 -0700

    correct setup

--- perf/wrapall/tests.js
+++ perf/wrapall/tests.js
@@ -1,15 +1,13 @@
 var suite = new Benchmark.Suite, i = 0;
 
 Benchmark.prototype.setup = function() {
-	// cache the test sets
-	window.testSetCurrent = jQuery( "[data-test]" );
-	window.testSetSplit = jqsplit( "[data-test]" );
+  var selector = window.testSetCurrent.selector,
+        clone = window.testSetCurrent.clone();
 
-	if( !window.testSetCurrent.parent().is( "body") ){
-		var wrapper = window.testSetCurrent.parent();
-		window.testSetCurrent.appendTo( "body" );
-		wrapper.remove();
-	}
+    old( ".cleanup" ).remove();
+    clone.appendTo( "body" );
+    window.testSetCurrent = old( "[data-test]" );
+    window.testSetSplit = jqsplit( "[data-test]" );
 };
 
 // add tests
