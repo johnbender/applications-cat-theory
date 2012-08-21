91510a9655e4f9338e2029fddbd7fa9066c7a2ab
John Bender <john.m.bender@gmail.com>
2012-08-21 16:06:56 -0700

    attempt to mitigate cleanup overhead

--- perf/wrapall/tests.js
+++ perf/wrapall/tests.js
@@ -5,12 +5,12 @@ suite
 	.add( 'wrapAll with string (1.8)', function(){
 		window.createTestSet( 1 );
 		window.testSetCurrent.wrapAll( "<div class='cleanup'></div>" );
-		$( ".cleanup" ).remove();
+		jqsplit.remove( document.querySelector( '.cleanup' ));
 	})
-	.add( 'wrapAll with string (split)', function() {n
+	.add( 'wrapAll with string (split)', function() {
 		window.createTestSet( 1 );
 		window.testSetSplit.wrapAll( "<div class='cleanup'></div>" );
-		$( ".cleanup" ).remove();
+		jqsplit.remove( document.querySelector( '.cleanup' ));
 	})
 	.add( 'wrapAll with element (core)', function() {
 		var wrapper;
@@ -19,7 +19,7 @@ suite
 		wrapper = document.createElement( "div" );
 		wrapper.setAttribute( "class", "cleanup" );
 		jqsplit.wrapAll( window.testSetCurrent, wrapper );
-		$( ".cleanup" ).remove();
+		jqsplit.remove( document.querySelector( '.cleanup' ));
 	})
 	.on('complete', function() {
 		$( "#results" ).text('Results: Fastest is ' + this.filter('fastest').pluck('name'));
